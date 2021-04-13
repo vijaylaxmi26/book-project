fd2bc9c8577bba0ba265366887581e0c313452c5
vijaylaxmi26 <68533681+vijaylaxmi26@users.noreply.github.com>
2021-04-13 14:26:57 +0530

    common header and footer

---
+++ Book project/includes/header.php
@@ -0,0 +1,12 @@
+<?php  require_once("./includes/db1.php"); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Book Store</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/style.css">
+
+      
+    </head>
+    <body>
