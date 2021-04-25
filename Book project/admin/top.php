f383423fcaee5a3f5f792c24995535b6e0b4ae8c
vijaylaxmi26 <68533681+vijaylaxmi26@users.noreply.github.com>
2021-04-25 10:32:08 +0530

    contact us work

--- Book project/admin/top.php
+++ Book project/admin/top.php
@@ -49,7 +49,7 @@ if(isset($_SESSION['ADMIN_LOGIN']) && $_SESSION['ADMIN_LOGIN']!=''){
                      <a href="#" > user Master</a>
                   </li>
                   <li class="menu-item-has-children dropdown">
-                     <a href="#" > Contact Us</a>
+                     <a href="contact_us.php" > Contact Us</a>
                   </li>
                </ul>
             </div>
